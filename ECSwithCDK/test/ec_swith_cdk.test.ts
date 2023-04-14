c8ac83421ddfcd1d01807d52be7a6e1fceef2606
Tejwani <rtejwani@b0be836c5e1c.ant.amazon.com>
2023-04-14 16:53:55 +0530

    The updated code with all the changes except Readme.md

--- ECSwithCDK/test/ec_swith_cdk.test.ts
+++ ECSwithCDK/test/ec_swith_cdk.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as MarcoProjectCdkPart from '../lib/marco_project_cdk_part-stack';
+// import * as EcSwithCdk from '../lib/ec_swith_cdk-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/marco_project_cdk_part-stack.ts
+// example resource in lib/ec_swith_cdk-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new MarcoProjectCdkPart.MarcoProjectCdkPartStack(app, 'MyTestStack');
+//   const stack = new EcSwithCdk.EcSwithCdkStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
